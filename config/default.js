6c4a4e820d1a288f05670478974531840179f48c
Daniel Wang <danielwpz@gmail.com>
2022-01-26 12:21:45 +0800

    change rpc

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ module.exports = {
   "networkConfig": {
     "mainnet": {
       "id": "mainnet",
-      "rpc": "https://public-rpc.blockpi.io/http/near"
+      "rpc": "https://rpc.mainnet.near.org"
     },
     "testnet": {
       "id": "testnet",
